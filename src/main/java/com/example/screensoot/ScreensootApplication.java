55c9fc8001a1b676b1b8f856c66f5640601fab96
Wangjunwei <vslegend@foxmail.com>
2019-11-21 09:41:41 +0800

    音视频工具

---
+++ src/main/java/com/example/screensoot/ScreensootApplication.java
@@ -0,0 +1,13 @@
+package com.example.screensoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreensootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreensootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/screensoot/ScreensootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.screensoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreensootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
