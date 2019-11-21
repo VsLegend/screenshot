fdf5d72c9a4c79cb169ef770c4116015b2554a3d
Wangjunwei <vslegend@foxmail.com>
2019-11-21 18:02:35 +0800

    本地视频帧截取

--- src/main/java/com/example/screenshot/utils/ScreemShot.java
+++ src/main/java/com/example/screenshot/utils/ScreemShot.java
@@ -1,56 +1,57 @@
 package com.example.screenshot.utils;
 
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import javax.imageio.ImageIO;
-import org.bytedeco.javacpp.opencv_core.IplImage;
+
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.FrameGrabber.Exception;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter.ToIplImage;
+import org.bytedeco.opencv.global.opencv_imgcodecs;
+import org.bytedeco.opencv.opencv_core.IplImage;
 
 /**
  * @author Wangjunwei
  * @Date 2019/11/20 17:45
- * @Description
+ * @Description 图片截取
  */
 public class ScreemShot {
 
   /**
-   * 获取指定视频的帧并保存为图片至指定目录
-   * @param videofile  源视频文件路径
-   * @param framefile  截取帧的图片存放路径
-   * @throws Exception
+   * 获取视频时长(单位：秒)
    */
-  public static void fetchFrame(String videofile, String framefile)
-      throws Exception {
-    long start = System.currentTimeMillis();
-    File targetFile = new File(framefile);
-    FFmpegFrameGrabber ff = new FFmpegFrameGrabber(videofile);
-    ff.start();
-    int lenght = ff.getLengthInFrames();
-    int i = 0;
-    Frame f = null;
-    while (i < lenght) {
-      // 过滤前5帧，避免出现全黑的图片，依自己情况而定
-      f = ff.grabFrame();
-      if ((i > 5) && (f.image != null)) {
-        break;
-      }
-      i++;
+  public static long getVideoLength(String sourcePath) throws Exception {
+    long length = 0;
+    FrameGrabber grabber = new FFmpegFrameGrabber(sourcePath);
+    grabber.start();
+    length = grabber.getLengthInTime() / (1000 * 1000);
+    grabber.stop();
+    return length;
+  }
+
+  /**
+   * 截取视频的一帧，并保存为图片bmp jpg jpeg png tiff等格式。 截取的图片大小为原视频的大小
+   */
+  public static void screenShot(String sourcePath, String targetPath) throws Exception {
+    FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(sourcePath);
+    // C语言图像类型
+    OpenCVFrameConverter.ToIplImage converter = new ToIplImage();
+    grabber.start();
+    int length = grabber.getLengthInFrames();
+    System.out.println("视频帧数：" + length);
+    if (1 > length) {
+      return;
     }
-    IplImage img = f.image;
-    int owidth = img.width();
-    int oheight = img.height();
-    // 对截取的帧进行等比例缩放
-    int width = 800;
-    int height = (int) (((double) width / owidth) * oheight);
-    BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-    bi.getGraphics().drawImage(f.image.getBufferedImage().getScaledInstance(width, height, Image.SCALE_SMOOTH),
-        0, 0, null);
-    ImageIO.write(bi, "jpg", targetFile);
-    //ff.flush();
-    ff.stop();
-    System.out.println(System.currentTimeMillis() - start);
+    Frame frame = null;
+    int i = 0;
+    // 获取一帧
+    do {
+      frame = grabber.grabImage();
+      i++;
+    } while (i < length && i < 50 && i < length / 1000); //尽量帧数小，避免占用资源，也要避开视频开始的黑屏
+    IplImage iplImage = converter.convertToIplImage(frame);
+    opencv_imgcodecs.cvSaveImage(targetPath, iplImage);
+    grabber.stop();
   }
 
 }
